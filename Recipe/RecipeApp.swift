172f6e41865ea9e685e05a7b306f3412c5fd575c
DonShan <msdjshan47@gmail.com>
2022-12-14 07:22:47 +0530

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Madushan Senavirathna on 2022-12-14.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
